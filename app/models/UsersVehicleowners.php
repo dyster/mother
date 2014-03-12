f1df4199e5cb4e494eab738b2959013cd995e3e8
Johan Sonesson <johan@tteknik.se>
2014-03-12 22:02:48 +0100

    new models

---
+++ app/models/UsersVehicleowners.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+
+class UsersVehicleowners extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $user_id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $vehicleowner_id;
+     
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'user_id' => 'user_id', 
+            'vehicleowner_id' => 'vehicleowner_id'
+        );
+    }
+
+}
---
+++ app/models/UsersVehicletypes.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+
+class UsersVehicletypes extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $user_id;
+     
+    /**
+     *
+     * @var integer
+     */
+    public $vehicletype_id;
+     
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'user_id' => 'user_id', 
+            'vehicletype_id' => 'vehicletype_id'
+        );
+    }
+
+}
